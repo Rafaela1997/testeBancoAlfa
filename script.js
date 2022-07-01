2649499c51207e7c02c653f2459c6a47933f9884
rafaela1997 <rafaelaramosdesigner@gmail.com>
2022-07-01 00:01:08 -0300

    foi feita a atualização do script a api ja esta sendo consumida e também foi feita a estilização do campo de busca porém falta o seu script e deixar o site responsivo

--- script.js
+++ script.js
@@ -1,26 +1,53 @@
 function apiBase() {
+    const list = document.getElementById('list')
+
     let url = 'https://pokeapi.co/api/v2/pokemon/';
     fetch(url)
         .then((Response) => {
             return Response.json();
         })
         .then((data) => {
-            console.log(data.results.name);
+           
 
+           const pokemons = data.results;
+           pokemons.map((pokemon,index)=>{
+               fetch(pokemon.url)
+                   .then((Response)=>{
+                       return Response.json();   
+                   }
+                   )
+                   .then((data) => {
+                       const img = data.sprites.front_default;
+                       const card = document.createElement('div')
+                       card.setAttribute("class","card")
+                       
+                       const contentText = document.createElement('div')
+                       contentText.setAttribute("class","contentText")
+                       const nomePokemon = document.createElement('span')
+                       nomePokemon.textContent = pokemon.name
+                       const idPokemon = document.createElement('span')
+                       idPokemon.textContent = "#"+ index + 1;
+                       
+                      const imgPokemon = document.createElement('img');
+                      imgPokemon.src = img
+                       console.log(imgPokemon)
+                       contentText.appendChild(nomePokemon)
+                       contentText.appendChild(idPokemon)
+                       card.appendChild(contentText)
+                       card.appendChild(imgPokemon)
+                        list.appendChild(card)
 
-            for (var i = 0; i <= 150; i++) {
+         
+                   })
+              })
+            
 
-                document.getElementById('nome').innerHTML = data.results[i]['name'];
-
-                document.getElementById('numero').innerHTML = i + 1;
-                
-            }
 
         })
 
 
         .catch((erro) => {
-            console.log("Erro" + erro)
+            console.log("Erro", erro)
         });
-    }
+}
 apiBase();
