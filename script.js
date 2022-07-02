156938b8d8039ea3f8f3e5b5d03200f8b3af9268
rafaela1997 <rafaelaramosdesigner@gmail.com>
2022-07-02 18:43:22 -0300

    O teste encontra-se pronto porém com alguns bugs

--- script.js
+++ script.js
@@ -11,41 +11,43 @@ function apiBase() {
 
     let url = `https://pokeapi.co/api/v2/pokemon?limit=150`;
     let urlFilter = `https://pokeapi.co/api/v2/pokemon/${imput}`
-    if(imput){
-        
+    if (imput) {
+
         fetch(urlFilter)
-        
-        .then((data)=>{
-            fetch(data.url)
-            .then((Response) => {
-                return Response.json();
-            })
+
             .then((data) => {
+                fetch(data.url)
+                    .then((Response) => {
+                        return Response.json();
+                    })
+                    .then((data) => {
 
-                console.log(data);
-                const img = data.sprites.front_default;
-                const card = document.createElement('div')
-                card.setAttribute("class", "card")
-                const contentText = document.createElement('div')
-                contentText.setAttribute("class", "contentText")
-                const nomePokemon = document.createElement('span')
-                nomePokemon.textContent = data.name
-                const idPokemon = document.createElement('span')
-                //idPokemon.textContent = "#" + index + 1;
-                const imgPokemon = document.createElement('img');
-                imgPokemon.src = img
-                contentText.appendChild(nomePokemon)
-                contentText.appendChild(idPokemon)
-                card.appendChild(contentText)
-                card.appendChild(imgPokemon)
-                list.appendChild(card)
-                
+                        console.log(data);
+                        const img = data.sprites.front_default;
+                        const card = document.createElement('div')
+                        card.setAttribute("class", "card")
+                        const contentText = document.createElement('div')
+                        contentText.setAttribute("class", "contentText")
+                        const nomePokemon = document.createElement('span')
+                        nomePokemon.textContent = data.name
+                        const idPokemon = document.createElement('span')
+                        //idPokemon.textContent = "#" + index + 1;
+                        const imgPokemon = document.createElement('img');
+                        imgPokemon.src = img
+                        contentText.appendChild(nomePokemon)
+                        contentText.appendChild(idPokemon)
+                        card.appendChild(contentText)
+                        card.appendChild(imgPokemon)
+                        list.appendChild(card)
+
+
+
+                    })
+
+
+
+                var li = list.setAttribute.nomePokemon;
             })
-
-
-
-            var li = list.setAttribute.nomePokemon;
-        })
 
     }
     fetch(url)//Faz o GET na API, o fetch tem o GET como metodo padrão
@@ -76,6 +78,8 @@ function apiBase() {
                         card.appendChild(contentText)
                         card.appendChild(imgPokemon)
                         list.appendChild(card)
+
+
                     })
             })
         })
