444182bd19f4a1688347193def060ac8f41e42e0
rafaela1997 <rafaelaramosdesigner@gmail.com>
2022-07-02 20:04:41 -0300

    O teste encontra-se pronto

--- script.js
+++ script.js
@@ -1,55 +1,9 @@
-function PegarValor() {
-    console.log(imput)
-}
-
-function apiBase() {
-    var imput = document.getElementById('buscaTxt').value;
-    console.log(imput)
+function reset() {
     const list = document.getElementById('list')//Cria o elemento list e solicita que ele pege o elemento list no HTML
-    const buscaTxt = document.getElementById('buscaTxt')
-    const btnBusca = document.getElementById('btnBusca')
+    list.innerHTML = "";
 
     let url = `https://pokeapi.co/api/v2/pokemon?limit=150`;
-    let urlFilter = `https://pokeapi.co/api/v2/pokemon/${imput}`
-    if (imput) {
-
-        fetch(urlFilter)
-
-            .then((data) => {
-                fetch(data.url)
-                    .then((Response) => {
-                        return Response.json();
-                    })
-                    .then((data) => {
-
-                        console.log(data);
-                        const img = data.sprites.front_default;
-                        const card = document.createElement('div')
-                        card.setAttribute("class", "card")
-                        const contentText = document.createElement('div')
-                        contentText.setAttribute("class", "contentText")
-                        const nomePokemon = document.createElement('span')
-                        nomePokemon.textContent = data.name
-                        const idPokemon = document.createElement('span')
-                        //idPokemon.textContent = "#" + index + 1;
-                        const imgPokemon = document.createElement('img');
-                        imgPokemon.src = img
-                        contentText.appendChild(nomePokemon)
-                        contentText.appendChild(idPokemon)
-                        card.appendChild(contentText)
-                        card.appendChild(imgPokemon)
-                        list.appendChild(card)
-
-
-
-                    })
-
-
-
-                var li = list.setAttribute.nomePokemon;
-            })
-
-    }
+    
     fetch(url)//Faz o GET na API, o fetch tem o GET como metodo padrão
         .then((Response) => {
             return Response.json();
@@ -70,7 +24,7 @@ function apiBase() {
                         const nomePokemon = document.createElement('span')
                         nomePokemon.textContent = pokemon.name
                         const idPokemon = document.createElement('span')
-                        idPokemon.textContent = "#" + index + 1;
+                        idPokemon.textContent = "#" + data.id;
                         const imgPokemon = document.createElement('img');
                         imgPokemon.src = img
                         contentText.appendChild(nomePokemon)
@@ -78,13 +32,60 @@ function apiBase() {
                         card.appendChild(contentText)
                         card.appendChild(imgPokemon)
                         list.appendChild(card)
-
-
                     })
             })
         })
         .catch((erro) => {
             console.log("Erro", erro)
         });
+}
+
+function apiBase() {
+    const input = document.getElementById('buscaTxt');
+
+    reset();
+
+    input.addEventListener("input", function () {
+        const list = document.getElementById('list')//Cria o elemento list e solicita que ele pege o elemento list no HTML
+        const value = input.value;
+
+        if (value === "") {
+            reset();
+            return;
+        }
+
+        let urlFilter = `https://pokeapi.co/api/v2/pokemon/${value}`
+        fetch(urlFilter)//Faz o GET na API, o fetch tem o GET como metodo padrão
+            .then((Response) => {
+                if (Response.status === 200) {
+                    return Response.json();
+                }
+
+                throw new Error('Erro na requisição')
+            })
+            .then((data) => {
+                list.innerHTML = "";
+
+                const img = data.sprites.front_default;
+                const card = document.createElement('div')
+                card.setAttribute("class", "card")
+                const contentText = document.createElement('div')
+                contentText.setAttribute("class", "contentText")
+                const nomePokemon = document.createElement('span')
+                nomePokemon.textContent = data.name
+                const idPokemon = document.createElement('span')
+                idPokemon.textContent = "#" + data.id;
+                const imgPokemon = document.createElement('img');
+                imgPokemon.src = img
+                contentText.appendChild(nomePokemon)
+                contentText.appendChild(idPokemon)
+                card.appendChild(contentText)
+                card.appendChild(imgPokemon)
+                list.appendChild(card)
+            })
+            .catch(() => {
+                return null;
+            })
+    })
 }
 apiBase();
